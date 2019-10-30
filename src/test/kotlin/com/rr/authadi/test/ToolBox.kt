044a60d923bdf99c13258553c9c9cd8fbe1e1603
Ramesh Rajamani <ramesh.rajamani@gmail.com>
2019-10-30 17:15:36 +0530

    Clean up flyway migration before test, simplified version

--- src/test/kotlin/com/rr/authadi/test/ToolBox.kt
+++ src/test/kotlin/com/rr/authadi/test/ToolBox.kt
@@ -2,25 +2,31 @@ package com.rr.authadi.test
 
 import com.rr.authadi.ServiceRunner
 import com.rr.authadi.injection.component.DaggerServiceComponent
-import com.rr.authadi.injection.component.ServiceComponent
 import com.rr.authadi.injection.module.ServiceModule
 import com.rr.authadi.setup.AppConfig
 import com.rr.authadi.setup.JdbiHandle
-import com.rr.authadi.test.config.FlywayConfig
+import org.flywaydb.core.Flyway
 import org.jdbi.v3.core.Jdbi
 
 object ToolBox {
     private val dbConfig = AppConfig.dbProperties()
-    private val flywayConfig by lazy {
-        FlywayConfig(dbConfig).config
-    }
-    @JvmStatic lateinit var serviceComponent: ServiceComponent
     val dataHandle: Jdbi
     init {
         ServiceRunner.serviceComponent = DaggerServiceComponent.builder()
                 .serviceModule(ServiceModule())
                 .build()
         dataHandle = JdbiHandle().getJdbiHandle()
-        flywayConfig.migrate()
+        runMigration()
+    }
+
+    private fun runMigration() {
+        val props = AppConfig.dbProperties()
+        val flyway: Flyway = Flyway.configure()
+                .dataSource(
+                        "${props.get("db.url")}?currentSchema=${props.get("db.schemas")}",
+                        props.get("db.user"),
+                        props.get("db.password")
+                ).load()
+        flyway.migrate()
     }
 }
